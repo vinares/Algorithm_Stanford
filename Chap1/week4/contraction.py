c8218add629c23c8deda76622f7cc86a6a264c88
vinares <877649518@qq.com>
2021-07-06 20:49:05 +0800

    edges contraction

--- Chap1/week4/contraction.py
+++ Chap1/week4/contraction.py
@@ -1,8 +1,8 @@
 import numpy as np
 import copy
 import time
+import math
 count = 0
-
 
 def contraction(Graph):
     n = len(Graph)
@@ -11,22 +11,18 @@ def contraction(Graph):
         return Graph[vertices[0]][vertices[1]], (vertices[0], vertices[1])
     vertex_a = np.random.choice(vertices)
     edges_a = list(Graph[vertex_a].keys())
+
     vertex_b = np.random.choice(edges_a)
     edges_b = list(Graph[vertex_b].keys())
-
     for contact in edges_b:
-        if contact != vertex_a:
+        if contact != vertex_a and contact != vertex_b:
             if contact not in Graph[vertex_a]:
                 Graph[vertex_a][contact] = Graph[vertex_b][contact]
+                Graph[contact][vertex_a] = Graph[contact][vertex_b]
             else:
                 Graph[vertex_a][contact] += Graph[vertex_b][contact]
-            if vertex_a not in Graph[contact]:
-                Graph[contact][vertex_a] = Graph[vertex_b][vertex_a]
-            else:
-                Graph[contact][vertex_a] += Graph[vertex_b][vertex_a]
+                Graph[contact][vertex_a] = Graph[vertex_a][contact]
             del Graph[contact][vertex_b]
-
-
     del Graph[vertex_a][vertex_b]
     if vertex_a in Graph[vertex_a]:
         del Graph[vertex_a][vertex_a]
@@ -37,11 +33,11 @@ def minCut(graph):
     global count
 
     n = len(graph.keys())
-    N = int(n ** 2 * np.log(n))
-    crossing_edges = len(graph[1])
+    N = int(n ** 2 * math.log(n))
+    crossing_edges = n * (n - 1) // 2
     minimum_cuts = set()
 
-    for _ in range(N):
+    for _ in range(n * n):
         copygraph = copy.deepcopy(graph)
         count += 1
         if count % 1000 == 0:
@@ -51,6 +47,7 @@ def minCut(graph):
             crossing_edges = cur_cross
             minimum_cuts = set()
             minimum_cuts.add(cur_cuts)
+            print(cur_cross, minimum_cuts)
         elif cur_cross == crossing_edges:
             minimum_cuts.add(cur_cuts)
     return crossing_edges, minimum_cuts
@@ -58,8 +55,8 @@ def minCut(graph):
 print(count)
 
 
-
 file = open('kargerMinCut.txt','r',)
+#file = open('1.txt','r',)
 graph = dict()
 
 for line in file:
@@ -69,6 +66,7 @@ for line in file:
     graph[cur[0]] = dict()
     for i in range(1,len(cur)):
         graph[cur[0]][cur[i]] = 1
+
 
 tic = time.time()
 print(minCut(graph))
