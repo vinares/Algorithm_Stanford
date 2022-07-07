c75b9d8669adbaf5352792cbbb652b7937307050
Yuchen Liu <74889158+vinares@users.noreply.github.com>
2022-07-06 21:31:10 -0700

    Update 2ndLargestNumber.py

--- Chap1/week2/2ndLargestNumber.py
+++ Chap1/week2/2ndLargestNumber.py
@@ -3,28 +3,43 @@ You are given as input an unsorted array of n distinct numbers, where n is a pow
 Give an algorithm that identifies the second-largest number in the array, and that uses
 at most   n + log(n) - 2   comparisons.
 """
-
+from collections import defaultdict
+from lib2to3.pytree import HUGE
 import numpy as np
 def divideAndConquer(nums):
-    count = 0
-    def find2nd(nums):
-        nonlocal count
+    ht = defaultdict(list)
+    def find1st(nums):
         n = len(nums)
+        if n == 0:
+            return -1, 0
+        if n == 1:
+            return nums[0], 0
         if n == 2:
-            count += 1
-            return (min(nums), max(nums))
+            if nums[0] > nums[1]:
+                ht[nums[0]].append(nums[1])
+                return nums[0], 1
+            else:
+                ht[nums[1]].append(nums[0])
+                return nums[1], 1
+        A, a = find1st(nums[0:n // 2])
+        B, b = find1st(nums[n // 2:n])
+        if A > B:
+            ht[A].append(B)
+            return A, a + b + 1
+        else:
+            ht[B].append(A)
+            return B, a + b + 1
+    M, c = find1st(nums)
+    MM, cc= find1st(ht[M])
+    return MM, c + cc
 
-        A = find2nd(nums[0:n // 2])
-        B = find2nd(nums[n // 2:n])
-        count += 1
-        return (min(A[1], B[1]), max(A[1], B[1]))
-    return find2nd(nums)[0], count
 
-n = 10
+n = 100
 np.random.seed(10)
-nums = np.random.randn(2 ** n)
+nums = [i for i in range(1, n+1)]
+np.random.shuffle(nums)
 sortedlist = nums.copy()
 sortedlist.sort()
 a = divideAndConquer(nums)
-print(a[0] == sortedlist[-2])
-print(a[1] <= n + 2 ** n - 2)
+assert a[0] == sortedlist[-2]
+assert a[1] <= n + np.ceil(np.log2(n)) - 2
